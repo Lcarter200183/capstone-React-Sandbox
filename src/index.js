682a842aa1b42941b698b5d261942aff3e57bfa4
Lydia Carter <Lcarter200183@yahoo.com>
2025-02-05 13:43:04 -0600

    added file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import { React } from "react"
 import { createRoot } from "react-dom/client"
+import { BrowserRouter } from "react-router";
+import { Routes } from "react-router";
+import { Route } from "react-router";
+import { Home } from "../Views/Home";
 
-const root = createRoot(window.bodyTag);
-root.render()
+const bodyTag = document.getElementById("bodyTag");
+const root = createRoot(bodyTag);
+ root.render(
+<BrowserRouter>
+<Routes>
+    <Route />
+</Routes>
+</BrowserRouter>
+);
